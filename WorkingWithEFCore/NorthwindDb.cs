40c07da06e0ce55d94cd824b0ce1856c0b3e6438
Peter Stackebrandt <31069750+pstackebrandt@users.noreply.github.com>
2024-05-02 22:06:49 +0200

    Add connection to Northwind DB.

---
+++ WorkingWithEFCore/NorthwindDb.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;    // To use DbContext and more
+
+namespace Northwind.EntityModels;
+
+/** This manages the interaction with the Northwind database.**/
+public class NorthwindDb : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string databaseFile = "Northwind.db";
+        string path =  Path.Combine(Environment.CurrentDirectory, databaseFile);
+        string connectionString = $"Data Source={path}";
+        WriteLine($"connectionString: {connectionString}");
+        optionsBuilder.UseSqlite(connectionString);
+    }
+}
--- WorkingWithEFCore/Program.cs
+++ WorkingWithEFCore/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Northwind.EntityModels; // To use NorthwindDb
+
+using NorthwindDb db = new();
+WriteLine($"Provider: {db.Database.ProviderName}");
+// Prints: "Provider: Microsoft.EntityFrameworkCore.Sqlite"
+
+// Disposes the database context. (not implemented)
